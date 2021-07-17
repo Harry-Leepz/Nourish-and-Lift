c913cd38c172cabe91312f9678383da3617699bb
Harry-Leepz <hdhillon478@gmail.com>
2021-07-17 11:33:04 +0000

    Add url for products, Add views to render products template, Create directory for products.html template

--- products/views.py
+++ products/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from.models import Product
 
-# Create your views here.
+
+def all_products(request):
+    """
+    A view to return the products page,
+    including sorting and search queries
+    """
+
+    products = Product.objects.all()
+
+    context = {
+        'products': products,
+    }
+
+    return render(request, 'products/products.html', context)
