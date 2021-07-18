cf1d0549a7fd246fea2dd506b0a4ad9d16218700
Harry-Leepz <hdhillon478@gmail.com>
2021-07-18 15:10:32 +0000

    Add product detail template, Add view to show product detail page, update product details links and Update urls.py for product detail url

--- products/views.py
+++ products/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from.models import Product
 
 
@@ -15,3 +15,17 @@ def all_products(request):
     }
 
     return render(request, 'products/products.html', context)
+
+
+def product_detail(request, product_id):
+    """
+    A view to return the dedicated product page with product description
+    """
+
+    product = get_object_or_404(Product, pk=product_id)
+
+    context = {
+        'product': product,
+    }
+
+    return render(request, 'products/product_detail.html', context)
