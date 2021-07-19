cabe95311ebb60fe337a087ad4906f2313406dde
Harry-Leepz <hdhillon478@gmail.com>
2021-07-19 15:35:53 +0000

    Add search functionality to all products view, Add url path for actions for search forms

--- products/views.py
+++ products/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, reverse, redirect, get_object_or_404
+from django.contrib import messages
+from django.db.models import Q
 from.models import Product
 
 
@@ -9,6 +11,18 @@ def all_products(request):
     """
 
     products = Product.objects.all()
+    query = None
+
+    if request.GET:
+        if 'q' in request.GET:
+            query = request.GET['q']
+            if not query:
+                messages.error(
+                    request, "You have not entered any search criteria!")
+                return redirect(reverse('products'))
+
+            queries = Q(name__icontains=query) | Q(description__icontains=query)
+            products = products.filter(queries)
 
     context = {
         'products': products,
