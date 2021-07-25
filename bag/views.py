d12ca4b044ff58203982b763a9ccf287967f1208
Harry-Leepz <hdhillon478@gmail.com>
2021-07-25 09:02:21 +0000

    Add view for adding products to bag, Add url path for add_to_bag view, Update action with view url for products detail template form

--- bag/views.py
+++ bag/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 
 def view_bag(request):
@@ -8,3 +8,23 @@ def view_bag(request):
     """
 
     return render(request, 'bag/bag.html')
+
+
+def add_to_bag(request, item_id):
+    """
+    A view to all the users to add items to the bag
+    with a quantity of their choosing
+    """
+
+    redirect_url = request.POST.get('redirect_url')
+    quantity = int(request.POST.get('quantity'))
+    bag = request.session.get('bag', {})
+
+    if item_id in list(bag.keys()):
+        bag[item_id] += quantity
+    else:
+        bag[item_id] = quantity
+
+    request.session['bag'] = bag
+    print(request.session['bag'])
+    return redirect(redirect_url)
