94b9b643b2061f5dedb1ad7bf999b63cad8f9ada
Harry-Leepz <hdhillon478@gmail.com>
2021-08-10 07:38:10 +0000

    Update singals.py with fucntions for lineitem create/update/delete

--- checkout/signals.py
+++ checkout/signals.py
@@ -1,4 +1,22 @@
-from django.db.models.singals import post_save, post_delete
+from django.db.models.signals import post_save, post_delete
 from django.dispatch import receiver
 
 from .models import OrderLineItem
+
+
+@receiver(post_save, sender=OrderLineItem)
+def update_on_save(sender, instance, created, **kwargs):
+    """
+    Update the order total on line items when they are
+    updated or created
+    """
+    instance.order.update_total()
+
+
+@receiver(post_delete, sender=OrderLineItem)
+def update_on_delete(sender, instance, created, **kwargs):
+    """
+    Update the order total on line items when they are
+    removed
+    """
+    instance.order.update_total()
