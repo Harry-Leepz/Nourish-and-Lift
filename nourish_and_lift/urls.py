50a5f02adcc3ad32381389fa8b7723bc8377a628
Harry-Leepz <hdhillon478@gmail.com>
2021-08-10 08:05:50 +0000

    Add url path for checkout url, Update project url file with checkout url

--- nourish_and_lift/urls.py
+++ nourish_and_lift/urls.py
@@ -25,4 +25,5 @@ urlpatterns = [
     path('about/', include('about.urls')),
     path('products/', include('products.urls')),
     path('bag/', include('bag.urls')),
+    path('checkout/', include('checkout.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
