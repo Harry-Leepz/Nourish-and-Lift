872927b47ef011ea31021057a5f3200655fddd5a
Harry-Leepz <hdhillon478@gmail.com>
2021-08-10 08:01:23 +0000

    Add checkout functionality to render checkout template and form in checkout/views.py

--- checkout/views.py
+++ checkout/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse
+from django.contrib import messages
 
-# Create your views here.
+from .forms import OrderForm
+
+
+def checkout(request):
+    """
+    A view to retrieve the bag from the session, render the checkout template
+    and render the checkout form
+    """
+
+    bag = request.session.get('bag', {})
+    if not bag:
+        messages.error(request, "Your bag is empty!")
+        return redirect(reverse('products'))
+
+    order_form = OrderForm()
+    template = 'checkout/checkout/html'
+    context = {
+        'order_form': order_form,
+    }
+
+    return render(request, template, context)
