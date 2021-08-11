f22ddabf67880060b59642d0da97bff070d4cd4b
Harry-Leepz <hdhillon478@gmail.com>
2021-08-11 19:55:29 +0000

    Add Stripe elements and styling, Add script tag for static js file

--- checkout/views.py
+++ checkout/views.py
@@ -19,6 +19,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51JMwr4CFuR9VVP0475wE4qoQuaBeAlNoR3OwZj0mdqq9j6lGHBDZjEwIn3fCF7jBmLMF7DSSxhJCXjowLJOuL9Ha00viMINqYZ',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
