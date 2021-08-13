b8d5b2d546e35d426ca6e174acb452021dea292a
Harry-Leepz <hdhillon478@gmail.com>
2021-08-13 08:56:50 +0000

    Add cache_checkout_data view, Add url for new view to urls.py

--- checkout/urls.py
+++ checkout/urls.py
@@ -6,6 +6,10 @@ urlpatterns = [
     path('', views.checkout, name='checkout'),
     path('wh/', webhook, name='webhook'),
     path(
+        'cache_checkout_data/',
+        views.cache_checkout_data,
+        name='cache_checkout_data'),
+    path(
         'checkout_success/<order_number>',
         views.checkout_success,
         name='checkout_success'),
