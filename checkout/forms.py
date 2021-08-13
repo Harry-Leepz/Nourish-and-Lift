6921d8a961e156445b94dddc478fbaf0e5fd3538
Harry-Leepz <hdhillon478@gmail.com>
2021-08-13 19:52:45 +0000

    Adjust layout, Change loading overlay color, Change county placeholder text

--- checkout/forms.py
+++ checkout/forms.py
@@ -25,7 +25,7 @@ class OrderForm(forms.ModelForm):
             'town_or_city': 'Town or City',
             'street_address1': 'Street Address 1',
             'street_address2': 'Street Address 2',
-            'county': 'County',
+            'county': 'County, State or Locality',
         }
 
         self.fields['full_name'].widget.attrs['autofocus'] = True
