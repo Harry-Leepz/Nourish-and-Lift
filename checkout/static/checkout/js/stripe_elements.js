260073e1c0a51d10563c04c93247381b70eba12a
Harry-Leepz <hdhillon478@gmail.com>
2021-08-13 08:12:47 +0000

    Add billing and shipping data to payment intent object in stripe_elements.js

--- checkout/static/checkout/js/stripe_elements.js
+++ checkout/static/checkout/js/stripe_elements.js
@@ -61,7 +61,31 @@ form.addEventListener('submit', function(ev) {
     stripe.confirmCardPayment(clientSecret, {
         payment_method: {
             card: card,
-        }
+            billing_details: {
+                name: $trim(form.full_name.value),
+                phone: $trim(form.phone_number.value),
+                email: $trim(form.email.value),
+                address: {
+                    line1: $trim(form.street_address1.value),
+                    line2: $trim(form.street_address2.value),
+                    city: $trim(form.town_or_city.value),
+                    country: $trim(form.country.value),
+                    state: $trim(form.county.value),
+                }
+            }
+        },
+        shipping: {
+            name: $trim(form.full_name.value),
+            phone: $trim(form.phone_number.value),
+            address: {
+                line1: $trim(form.street_address1.value),
+                line2: $trim(form.street_address2.value),
+                city: $trim(form.town_or_city.value),
+                country: $trim(form.country.value),
+                postal_code: $trim(form.postcode.value),
+                state: $trim(form.county.value),
+            }
+        },        
     }).then(function(result) {
         if (result.error) {
             var errorDiv = document.getElementById('card-errors');
