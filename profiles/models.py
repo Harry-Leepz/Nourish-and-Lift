12e99b431480c2fb382d6d6203e69de557943776
Harry-Leepz <hdhillon478@gmail.com>
2021-08-14 19:30:03 +0000

    Build UserProfile model, Add UserProfile FK to order model

--- profiles/models.py
+++ profiles/models.py
@@ -1,3 +1,41 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from django_countries.fields import CountryField
 
-# Create your models here.
+
+class UserProfile(models.Model):
+    """
+    A user profile model for maintaining default
+    delivery information and order history.
+    """
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+    # Delivery Details
+    default_phone_number = models.CharField(
+        max_length=20, blank=True)
+    default_country = CountryField(
+        blank_label='Country *', null=True, blank=True)
+    default_postcode = models.CharField(max_length=20, blank=True)
+    default_town_or_city = models.CharField(
+        max_length=40, blank=True)
+    default_street_address1 = models.CharField(
+        max_length=80, blank=True)
+    default_street_address2 = models.CharField(
+        max_length=80, blank=True)
+    default_county = models.CharField(max_length=80, blank=True)
+
+    def __str__(self):
+        return self.user.username
+
+
+@receiver(post_save, sender=User)
+def create_or_update_user_profile(sender, instance, created, **kwargs):
+    """
+    Create or update the user profile
+    """
+    if created:
+        UserProfile.objects.create(user=instance)
+    # Existing users: just save the profile
+    instance.userprofile.save()
