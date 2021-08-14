2b930998997a391f47d46384efecf573d0ecf9f8
Harry-Leepz <hdhillon478@gmail.com>
2021-08-14 19:41:24 +0000

    Add profile template and static folder for css, Updated urls.py, Add view to render profile template

--- profiles/views.py
+++ profiles/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def profile(request):
+    """
+    A view to render the user's profile page
+    """
+
+    template = 'profiles/profile.html'
+    context = {}
+
+    return render(request, template, context)
