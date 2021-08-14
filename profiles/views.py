fcc03412b3b7620c78f92d864986be416e041819
Harry-Leepz <hdhillon478@gmail.com>
2021-08-14 21:47:18 +0000

    Updated profile link in base template, Tested userprofile view

--- profiles/views.py
+++ profiles/views.py
@@ -1,12 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+
+from .models import UserProfile
 
 
 def profile(request):
     """
     A view to render the user's profile page
     """
+    profile = get_object_or_404(UserProfile, user=request.user)
 
     template = 'profiles/profile.html'
-    context = {}
+    context = {
+        'profile': profile,
+    }
 
     return render(request, template, context)
