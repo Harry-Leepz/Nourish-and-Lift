bd681fb90db72d8f006eaeae3a41e40a10afa5a9
Harry-Leepz <hdhillon478@gmail.com>
2021-08-15 13:03:38 +0000

    Fix issue with profile country field default font color being black instead of light grey

---
+++ profiles/static/profiles/js/countryfield.js
@@ -0,0 +1,13 @@
+let countrySelected = $('#id_default_country').val();
+// Fix to the Profile countryfield dropwdown, when a country is selected the color of the font is changed to black
+if(!countrySelected) {
+    $('#id_default_country').css('color', '#aab7c4');
+};
+$('#id_default_country').change(function() {
+    countrySelected = $(this).val();
+    if(!countrySelected) {
+        $(this).css('color', '#aab7c4');
+    } else {
+        $(this).css('color', '#000');
+    }
+});
