4b9df09c3457382241a1a90138615d38ee4819c1
Harry-Leepz <hdhillon478@gmail.com>
2021-08-15 12:48:48 +0000

    Add forms.py with UserProfileForm, Update profile view, Build User proile template

--- profiles/models.py
+++ profiles/models.py
@@ -15,16 +15,16 @@ class UserProfile(models.Model):
     # Delivery Details
     default_phone_number = models.CharField(
         max_length=20, blank=True)
-    default_country = CountryField(
-        blank_label='Country *', null=True, blank=True)
-    default_postcode = models.CharField(max_length=20, blank=True)
-    default_town_or_city = models.CharField(
-        max_length=40, blank=True)
     default_street_address1 = models.CharField(
         max_length=80, blank=True)
     default_street_address2 = models.CharField(
         max_length=80, blank=True)
+    default_town_or_city = models.CharField(
+            max_length=40, blank=True)
     default_county = models.CharField(max_length=80, blank=True)
+    default_postcode = models.CharField(max_length=20, blank=True)
+    default_country = CountryField(
+        blank_label='Country', null=True, blank=True)
 
     def __str__(self):
         return self.user.username
