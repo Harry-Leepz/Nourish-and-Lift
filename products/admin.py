6f6ca43ba796388dc864fa203fc54821aa2a6c0c
Harry-Leepz <hdhillon478@gmail.com>
2021-08-16 10:18:54 +0000

    Add product rewview section to produc details template

--- products/admin.py
+++ products/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Product, Category
+from .models import Product, Category, ProductReview
 
 
 class ProductAdmin(admin.ModelAdmin):
@@ -31,3 +31,4 @@ class CategoryAdmin(admin.ModelAdmin):
 
 admin.site.register(Product, ProductAdmin)
 admin.site.register(Category, CategoryAdmin)
+admin.site.register(ProductReview)
