9a617a01350bf642c9d4086e540605f9bec7c5e2
Harry-Leepz <hdhillon478@gmail.com>
2021-08-17 15:33:46 +0000

    Add wishlist app urls, Add views to render wishlist template and add to wish

---
+++ wishlist/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.wishlist, name='wishlist'),
+    path('add_to_wishlist/<product_id>', views.add_to_wishlist, name='add_to_wishlist'),
+]
--- wishlist/views.py
+++ wishlist/views.py
@@ -1,3 +1,34 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+from profiles.models import UserProfile
+from products.models import Product
+from .models import WishList
+
+
+@login_required
+def wishlist(request):
+    """
+    A view to render the users wishlist
+    """
+    profile = get_object_or_404(UserProfile, user=request.user)
+    products = profile.wishlist.all()
+    template = 'wishlist/wishlist.html'
+    context = {
+        'products': products,
+        'profile': profile,
+    }
+
+    return render(request, template, context)
+
+
+@login_required
+def add_to_wishlist(request, product_id):
+    """
+    Add a product from the store to the
+    wishlist for the logged in user
+    """
+    product = get_object_or_404(Product, pk=product_id)
+
+    wishlist = WishList.objects.get_or_create(user=request.user)
+    wishlist.product.add(product)
