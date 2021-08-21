766c42cd3a812a3045d0e027c7c96fe830cd600d
Harry-Leepz <hdhillon478@gmail.com>
2021-08-21 20:17:09 +0000

    Add view remove_from_wishlist, Update urls.py, Update href in template

--- wishlist/urls.py
+++ wishlist/urls.py
@@ -3,5 +3,12 @@ from . import views
 
 urlpatterns = [
     path('', views.wishlist, name='wishlist'),
-    path('add_to_wishlist/<product_id>', views.add_to_wishlist, name='add_to_wishlist'),
+    path(
+        'add_to_wishlist/<product_id>',
+        views.add_to_wishlist,
+        name='add_to_wishlist'),
+    path(
+        'remove_from_wishlist/<product_id>',
+        views.remove_from_wishlist,
+        name='remove_from_wishlist'),
 ]
--- wishlist/views.py
+++ wishlist/views.py
@@ -1,11 +1,10 @@
 from django.shortcuts import render, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponseRedirect
-
-from django.contrib.auth.models import User
+from django.contrib import messages
 
 from products.models import Product
-from .models import WishList, WishListItem
+from .models import WishList
 
 
 @login_required
@@ -34,8 +33,26 @@ def add_to_wishlist(request, product_id):
     """
     product = get_object_or_404(Product, pk=product_id)
 
+    # Create a wishlist for the user if they don't have one
     wishlist, created = WishList.objects.get_or_create(user=request.user)
     # Add product to the wishlist
     wishlist.products.add(product)
+    messages.info(request, "A new product was added to your wishlist")
+
+    return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
+
+
+@login_required
+def remove_from_wishlist(request, product_id):
+    """
+    Add a product from the store to the
+    wishlist for the logged in user
+    """
+    wishlist = WishList.objects.get(user=request.user)
+    product = get_object_or_404(Product, pk=product_id)
+
+    # Remove product from the wishlist
+    wishlist.products.remove(product)
+    messages.info(request, "A product was removed from your wishlist")
 
     return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
