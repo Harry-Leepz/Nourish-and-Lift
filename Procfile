648182bcf12dac0e0cb053a1dd83914d4cb9fb84
Harry-Leepz <hdhillon478@gmail.com>
2021-08-23 14:15:48 +0000

    Add Procfile, Install gunicorn and update requirements.txt

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn nourish_and_lift.wsgi:application
--- nourish_and_lift/settings.py
+++ nourish_and_lift/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-w+)hcr#b5@k42%$v9#y%nrqzu)%efshuli-x4y3g9rmz4x!10w
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', ]
+ALLOWED_HOSTS = ['nourish-and-lift.herokuapp.com', 'localhost']
 
 
 # Application definition
@@ -121,12 +121,17 @@ WSGI_APPLICATION = 'nourish_and_lift.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-     'default': {
-         'ENGINE': 'django.db.backends.sqlite3',
-         'NAME': BASE_DIR / 'db.sqlite3',
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 
 # Password validation
