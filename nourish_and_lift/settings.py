67d346ca599312e1a43672c397de04a43c9127bd
Harry-Leepz <hdhillon478@gmail.com>
2021-08-23 14:26:19 +0000

    Add secret key and debug status to settings

--- nourish_and_lift/settings.py
+++ nourish_and_lift/settings.py
@@ -22,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-w+)hcr#b5@k42%$v9#y%nrqzu)%efshuli-x4y3g9rmz4x!10w'
+SECRET_KEY = os.environ.get('SCERET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['nourish-and-lift.herokuapp.com', 'localhost']
 
