ae7dad6dcd61b90da2a88c4313ea4f6f7d506867
Harry-Leepz <hdhillon478@gmail.com>
2021-08-26 11:37:33 +0000

    Attempting heroku deployment, pre push

--- nourish_and_lift/settings.py
+++ nourish_and_lift/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['nourish-and-lift.herokuapp.com', 'localhost']
 
@@ -122,17 +122,17 @@ WSGI_APPLICATION = 'nourish_and_lift.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-#if 'DATABASE_URL' in os.environ:
-#    DATABASES = {
-#        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
-#    }
-# else:
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+            }
+    }
 
 
 # Password validation
@@ -216,14 +216,14 @@ STRIPE_WH_SECRET = os.getenv('STRIPE_WH_SECRET', '')
 DEFAULT_FROM_EMAIL = 'nourishandlift@example.com'
 
 
-#if 'DEVELOPMENT' in os.environ:
-#   EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-#    DEFAULT_FROM_EMAIL = 'nourishandlift@example.com'
-#else:
-#    EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-#    EMAIL_USE_TLS = True
-#    EMAIL_PORT = 587
-#    EMAIL_HOST = 'smtp.gmail.com'
-#    EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
-#    EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASS')
-#    DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_HOST_USER')
+if 'DEVELOPMENT' in os.environ:
+    EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+    DEFAULT_FROM_EMAIL = 'nourishandlift@example.com'
+else:
+    EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+    EMAIL_USE_TLS = True
+    EMAIL_PORT = 587
+    EMAIL_HOST = 'smtp.gmail.com'
+    EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+    EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASS')
+    DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_HOST_USER')
